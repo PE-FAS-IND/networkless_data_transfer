4f44aa3cd2712074d19c89c024bd14e41babe243
fas-baeby <benjamin.aeby@imi-precision.com>
2025-04-23 14:16:11 +0200

    Transfert de tous les fichiers locaux

--- nldt_app/nldt_machine.py
+++ nldt_app/nldt_machine.py
@@ -43,16 +43,17 @@ class NLDT_Machine:
     def process_local_dir(self):
         self.file_collector.list_files()
         logger.info(self.file_collector.to_transfer)
-        self.file_collector.file_to_frames(self.file_collector.to_transfer[0])
-        for frame in self.file_collector.outbox:
-            # print(frame)
-            payload = frame.encode('utf-8') + b"\r\n"
-            
-            logger.info(payload)
-            self.conn.write(payload)
-            time.sleep(0.05)
-        # Empty outbox
-        self.file_collector.outbox = []
+        for filename in self.file_collector.to_transfer:
+            self.file_collector.file_to_frames(filename)
+            for frame in self.file_collector.outbox:
+                # print(frame)
+                payload = frame.encode('utf-8') + b"\r\n"
+                
+                logger.info(payload)
+                self.conn.write(payload)
+                time.sleep(0.05)
+            # Empty outbox
+            self.file_collector.outbox = []
         
 
 if __name__ == "__main__":
